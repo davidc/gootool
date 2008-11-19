136a2e272e81ac3c903c85b6c7ada213c1cdf518
david <david@3c02dcd8-5a9b-469f-af0c-e276f805f532>
2008-11-19 20:14:29 +0000

    added menu items for various help webpages

--- trunk/src/com/goofans/gootool/util/HyperlinkLaunchingListener.java
+++ trunk/src/com/goofans/gootool/util/HyperlinkLaunchingListener.java
@@ -1,23 +1,27 @@
 package com.goofans.gootool.util;
 
-import javax.swing.event.HyperlinkListener;
 import javax.swing.event.HyperlinkEvent;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.event.HyperlinkListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author David Croft (davidc@goofans.com)
  * @version $Id$
  */
-public class HyperlinkLaunchingListener implements HyperlinkListener
+public class HyperlinkLaunchingListener implements HyperlinkListener, ActionListener
 {
-  private static final Logger log = Logger.getLogger(HyperlinkLaunchingListener.class.getName());
+//  private static final Logger log = Logger.getLogger(HyperlinkLaunchingListener.class.getName());
 
   public void hyperlinkUpdate(HyperlinkEvent event)
   {
     if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
       URLLauncher.launchAndWarn(event.getURL(), null); // TODO not null
     }
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    URLLauncher.launchAndWarn(e.getActionCommand(), null);
   }
 }
