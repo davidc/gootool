3ee64f9da9b2b6a91b4d6f968b450de55352d82e
david <david@3c02dcd8-5a9b-469f-af0c-e276f805f532>
2009-01-05 18:32:12 +0000

    mac image encoding

--- trunk/src/com/goofans/gootool/io/MacGraphicFormat.java
+++ trunk/src/com/goofans/gootool/io/MacGraphicFormat.java
@@ -1,15 +1,13 @@
 package com.goofans.gootool.io;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.image.*;
 import java.io.*;
 import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
 import java.util.zip.Inflater;
-
-import sun.awt.image.ByteArrayImageSource;
 
 /**
  * Handles encoding and decoding of the Mac .png.binltl raster format.
@@ -46,6 +44,8 @@ public class MacGraphicFormat
       throw new EOFException("Short read on compressed data, expected " + compressedSize);
     }
 
+    is.close();
+
     Inflater inflater = new Inflater();
     inflater.setInput(compressedData);
 
@@ -64,17 +64,93 @@ public class MacGraphicFormat
 
     // TODO: colour seems to be slightly off (at least in screenshot)
 
-    ColorSpace colorSpace = ColorSpace.getInstance(ColorSpace.CS_sRGB);
-    int[] pixInfo = new int[]{8, 8, 8, 8};
-    ComponentColorModel colorModel = new ComponentColorModel(colorSpace, pixInfo, true, false, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
-
-    int[] bandOffsets = new int[]{0, 1, 2, 3};
-    PixelInterleavedSampleModel sampleModel = new PixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, width, height, 4, squareSide * 4, bandOffsets);
+    ComponentColorModel colorModel = getColorModel();
+    PixelInterleavedSampleModel sampleModel = getSampleModel(width, height, squareSide);
 
     DataBufferByte imageData = new DataBufferByte(uncompressedData, uncompressedSize);
     WritableRaster raster = Raster.createWritableRaster(sampleModel, imageData, new Point(0, 0));
 
     return new BufferedImage(colorModel, raster, false, null);
+  }
+
+  private static PixelInterleavedSampleModel getSampleModel(int width, int height, int squareSide)
+  {
+    int[] bandOffsets = new int[]{0, 1, 2, 3};
+    PixelInterleavedSampleModel sampleModel = new PixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, width, height, 4, squareSide * 4, bandOffsets);
+    return sampleModel;
+  }
+
+  public static void encodeImage(File file, Image image) throws IOException
+  {
+    int width = image.getWidth(null);
+    System.out.println("width = " + width);
+    int height = image.getHeight(null);
+    System.out.println("height = " + height);
+
+    // Need to make a square first
+
+    int squareSide = 1;
+    while (squareSide < width || squareSide < height) squareSide *= 2;
+    System.out.println("squareSide = " + squareSide);
+
+    ComponentColorModel colorModel = getColorModel();
+    PixelInterleavedSampleModel sampleModel = getSampleModel(squareSide, squareSide, squareSide);
+
+    WritableRaster raster = Raster.createWritableRaster(sampleModel, new Point(0, 0));
+    BufferedImage squareImage = new BufferedImage(colorModel, raster, false, null);
+    squareImage.getGraphics().drawImage(image, 0, 0, null);
+
+    showImageWindow(squareImage);
+
+    // Get the RGBA bytes from this
+    DataBufferByte imageData = (DataBufferByte) squareImage.getRaster().getDataBuffer();
+
+    byte[] uncompressedData = imageData.getData();
+    int uncompressedSize = uncompressedData.length;
+
+    // Compress data
+
+    ByteArrayOutputStream bos = new ByteArrayOutputStream(4096);
+
+    Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION);
+    deflater.setInput(uncompressedData);
+    deflater.finish();
+    byte[] tmpbuf = new byte[1024];
+
+    int n;
+    while (!deflater.finished()) {
+      n = deflater.deflate(tmpbuf);
+      bos.write(tmpbuf, 0, n);
+    }
+    deflater.end();
+
+    byte[] compressedData = bos.toByteArray();
+    int compressedSize = compressedData.length;
+
+    // Now write to the file
+
+    OutputStream os = new FileOutputStream(file);
+
+    writeUnsignedShort(os, width);
+    writeUnsignedShort(os, height);
+    writeUnsignedInt(os, compressedSize);
+    System.out.println("compressedSize = " + compressedSize);
+    writeUnsignedInt(os, uncompressedSize);
+    System.out.println("uncompressedSize = " + uncompressedSize);
+
+    os.write(compressedData);
+
+    os.close();
+
+    deflater.end();
+  }
+
+  private static ComponentColorModel getColorModel()
+  {
+    ColorSpace colorSpace = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+    int[] pixInfo = new int[]{8, 8, 8, 8};
+    ComponentColorModel colorModel = new ComponentColorModel(colorSpace, pixInfo, true, false, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+    return colorModel;
   }
 
   private static void showImageWindow(BufferedImage image)
@@ -98,6 +174,12 @@ public class MacGraphicFormat
     return ((int) tmp[0] & 0xff) + (((int) tmp[1] & 0xff) << 8);
   }
 
+  private static void writeUnsignedShort(OutputStream os, int value) throws IOException
+  {
+    os.write(value & 0xff);
+    os.write((value >> 8) & 0xff);
+  }
+
   private static int readUnsignedInt(InputStream is) throws IOException
   {
     byte[] tmp = new byte[4];
@@ -106,11 +188,31 @@ public class MacGraphicFormat
     return ((int) tmp[0] & 0xff) + (((int) tmp[1] & 0xff) << 8) + (((int) tmp[2] & 0xff) << 16) + (((int) tmp[3] & 0xff) << 24);
   }
 
+  private static void writeUnsignedInt(OutputStream os, int value) throws IOException
+  {
+    os.write(value & 0xff);
+    os.write((value >> 8) & 0xff);
+    os.write((value >> 16) & 0xff);
+    os.write((value >> 24) & 0xff);
+  }
+
+  @SuppressWarnings({"HardCodedStringLiteral"})
   public static void main(String[] args) throws IOException
   {
-    showImageWindow(decodeImage(new File("bg.png.binltl")));
-    RenderedImage image = decodeImage(new File("cliff_left.png.binltl"));
+//    showImageWindow(decodeImage(new File("bg.png.binltl")));
 
-    ImageIO.write(image, "PNG", new File("cliff_left.png"));
+    BufferedImage image = decodeImage(new File("cliff_left.png.binltl"));
+
+    File tmpfile = new File("cliff_left-OUT.png.binltl");
+    encodeImage(tmpfile, image);
+    showImageWindow(decodeImage(tmpfile));
+
+    image = decodeImage(new File("cliff_right.png.binltl"));
+
+    tmpfile = new File("cliff_right-OUT.png.binltl");
+    encodeImage(tmpfile, image);
+    showImageWindow(decodeImage(tmpfile));
+
+//    ImageIO.write(image, "PNG", new File("cliff_left.png"));
   }
 }
