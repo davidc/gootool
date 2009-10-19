745a766da59352d508c682c8322680d08253c56d
david <david@3c02dcd8-5a9b-469f-af0c-e276f805f532>
2009-10-19 14:52:42 +0000

    - Skip Thumbs.db, .DS_Store and .svn when installing addins (#0000215) - Make AddinInstaller use AddinReader, allowing installation of an addin from an extracted directory.

--- trunk/src/com/goofans/gootool/addins/AddinReader.java
+++ trunk/src/com/goofans/gootool/addins/AddinReader.java
@@ -3,12 +3,12 @@ package com.goofans.gootool.addins;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * AddinFileReader should be used for all access to goomod files, since it transparently handles the switch to an extracted
  * directory where necessary. It's a thin facade around the various IO operations.
- * <p/>
- * TODO: Make AddinInstaller use this.
  *
  * @author David Croft (davidc@goofans.com)
  * @version $Id$
@@ -24,6 +24,16 @@ public interface AddinReader
    * @throws java.io.IOException           if the file could not be read for another reason.
    */
   public InputStream getInputStream(String fileName) throws FileNotFoundException, IOException;
+
+  /**
+   * Gives an iterator for all the files that are descendants of the given directory name (including files in subdirectories).
+   * Skips all files and directories on the skip list.
+   *
+   * @param directory The directory to search under, including trailing "/".
+   * @param skip A list of file/directory names to skip
+   * @return An iterator of valid files.
+   */
+  public Iterator<String> getEntriesInDirectory(String directory, List<String> skip);
 
   /**
    * Closes the addin once work is complete.
--- trunk/src/com/goofans/gootool/addins/ExpandedAddinReader.java
+++ trunk/src/com/goofans/gootool/addins/ExpandedAddinReader.java
@@ -1,6 +1,10 @@
 package com.goofans.gootool.addins;
 
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Arrays;
 
 /**
  * Reads an addin expanded on disk.
@@ -11,6 +15,7 @@ import java.io.*;
 public class ExpandedAddinReader implements AddinReader
 {
   File rootDirectory;
+  private File[] dirFiles;
 
   public ExpandedAddinReader(File rootDirectory)
   {
@@ -29,7 +34,47 @@ public class ExpandedAddinReader implements AddinReader
     return new FileInputStream(file);
   }
 
+  public Iterator<String> getEntriesInDirectory(String directory, List<String> skip)
+  {
+    File dir = new File(rootDirectory, directory);
+    List<String> entries = new ArrayList<String>();
+
+    if (dir.isDirectory()) {
+      getEntries(dir, null, entries, skip);
+    }
+
+    return entries.iterator();
+  }
+
+  private void getEntries(File dir, String prefix, List<String> entries, List<String> skip)
+  {
+    dirFiles = dir.listFiles();
+    for (File file : dirFiles) {
+      if (!skip.contains(file.getName())) {
+        String fileName = (prefix != null ? fileName = prefix + "/" : "") + file.getName();
+
+        if (file.isDirectory()) {
+          getEntries(file, fileName, entries, skip);
+        }
+        else if (file.isFile()) {
+          entries.add(fileName);
+        }
+      }
+    }
+  }
+
   public void close() throws IOException
   {
+  }
+
+  @SuppressWarnings({"UseOfSystemOutOrSystemErr", "HardCodedStringLiteral", "HardcodedFileSeparator", "DuplicateStringLiteralInspection"})
+  public static void main(String[] args)
+  {
+    AddinReader addinReader = new ExpandedAddinReader(new File("addins/src/com.goofans.davidc.jingleballs"));
+    Iterator<String> entries = addinReader.getEntriesInDirectory("override/", Arrays.asList(".svn"));
+    while (entries.hasNext()) {
+      String s = entries.next();
+      System.out.println("s = " + s);
+    }
   }
 }
