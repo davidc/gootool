e009b7093a6dfd1b9fd9063508c01adc29d8d8b9
davidc <davidc@users.noreply.github.com>
2010-05-02 18:03:02 +0000

    mass copyright insertion

--- src/com/goofans/gootool/util/FileNameExtensionFilter.java
+++ src/com/goofans/gootool/util/FileNameExtensionFilter.java
@@ -1,8 +1,6 @@
 /*
- * @(#)FileNameExtensionFilter.java	1.1 06/02/02
- *
- * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright (c) 2008, 2009, 2010 David C A Croft. All rights reserved. Your use of this computer software
+ * is permitted only in accordance with the GooTool license agreement distributed with this file.
  */
 
 package com.goofans.gootool.util;
