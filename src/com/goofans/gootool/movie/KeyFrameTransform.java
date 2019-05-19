b0e3b6dbdd538749485c85f138632a87a062529a
davidc <davidc@users.noreply.github.com>
2019-05-20 00:59:59 +0200

    update copyright year in source files

--- src/com/goofans/gootool/movie/KeyFrameTransform.java
+++ src/com/goofans/gootool/movie/KeyFrameTransform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2009, 2010 David C A Croft. All rights reserved. Your use of this computer software
+ * Copyright (c) 2008, 2009, 2010, 2019 David C A Croft. All rights reserved. Your use of this computer software
  * is permitted only in accordance with the GooTool license agreement distributed with this file.
  */
 
